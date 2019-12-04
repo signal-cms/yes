d39f6dfe656f167dbb2e3d33014ae2e7baf99e06
Maosen Chen <cms199631@163.com>
2019-12-04 11:07:26 +0800

    add amethon in models.py to update data having same date with beening tested

--- AoiDataAnalysisSystem/daily/models.py
+++ AoiDataAnalysisSystem/daily/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+# record daily data
+class DayData(models.Model):
+    year = models.IntegerField()
+    rst_date = models.DateField(db_column='date')
+    miss = models.IntegerField()
+    overkill = models.IntegerField()
+    useless = models.IntegerField()
+    aoi_in = models.IntegerField(db_column='AOIIn')
+    aoi_ok = models.IntegerField(db_column='AOIOk')
+    aoi_ng = models.IntegerField(db_column='AOINg')
+    fi_in = models.IntegerField(db_column='FIIn')
+    miss_rate = models.FloatField(db_column='MissRate')
+    overkill_rate = models.FloatField(db_column='OverkillRate')
+    useless_rate = models.FloatField(db_column='UselessRate')
+    miss_aoi = models.FloatField(db_column='MissRateAoiOk')
+    overkill_aoi = models.FloatField(db_column='OverkillRateAoiNg')
+
+    @classmethod
+    def create_day(cls, rst_date, miss, overkill, useless, aoi_in, aoi_ng, fi_in):
+        if DayData.objects.filter(rst_date=rst_date).exists():
+            data = DayData.objects.get(rst_date=rst_date)
+            data.miss += miss
+            data.overkill += overkill
+            data.useless += useless
+            data.aoi_in += aoi_in
+            data.aoi_ok += aoi_in - aoi_ng
+            data.aoi_ng += aoi_ng
+            data.fi_in += fi_in
+            data.miss_rate = data.miss / data.aoi_in
+            data.overkill_rate = data.overkill / data.aoi_in
+            data.useless_rate = data.useless / data.aoi_in
+            data.miss_aoi = data.miss / (data.fi_in - data.aoi_ng)
+            data.overkill_aoi = data.overkill / (data.aoi_ng - data.useless)
+            data.save()
+        else:
+            aoi_ok = aoi_in - aoi_ng
+            year = rst_date.year
+            miss_rate = miss / aoi_in
+            overkill_rate = overkill / aoi_in
+            useless_rate = useless / aoi_in
+            miss_aoi = miss / (fi_in - aoi_ng)
+            overkill_aoi = overkill / (aoi_ng - useless)
+            data = cls(year=year, rst_date=rst_date, miss=miss, overkill=overkill, useless=useless, aoi_in=aoi_in,
+                       aoi_ok=aoi_ok, aoi_ng=aoi_ng, fi_in=fi_in, miss_rate=miss_rate, overkill_rate=overkill_rate,
+                       useless_rate=useless_rate, miss_aoi=miss_aoi, overkill_aoi=overkill_aoi)
+            data.save()
+        return data
--- AoiDataAnalysisSystem/daily/tests.py
+++ AoiDataAnalysisSystem/daily/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import DayData
+from datetime import date
 
-# Create your tests here.
+
+class DailyModelTests(TestCase):
+
+    # Test create method
+    def test_create_day_normal(self):
+        data = DayData.create_day(date(2019, 11, 30), 10, 1500, 50, 10000, 2000, 3000)
+        self.assertEqual(data.year, 2019)
+        self.assertEqual(data.rst_date, date(2019, 11, 30))
+        self.assertEqual(data.miss, 10)
+        self.assertEqual(data.miss_rate, 10/10000)
+        self.assertEqual(data.miss_aoi, 10/1000)
+        self.assertEqual(data.overkill, 1500)
+        self.assertEqual(data.overkill_rate, 1500/10000)
+        self.assertEqual(data.overkill_aoi, 1500/1950)
+        self.assertEqual(data.useless, 50)
+        self.assertEqual(data.useless_rate, 50/10000)
+        self.assertEqual(data.aoi_in, 10000)
+        self.assertEqual(data.aoi_ng, 2000)
+        self.assertEqual(data.aoi_ok, 8000)
+        self.assertEqual(data.fi_in, 3000)
+
+    def test_create_day_exists(self):
+        DayData.create_day(date(2019, 11, 30), 10, 1500, 50, 10000, 2000, 3000)
+        data = DayData.create_day(date(2019, 11, 30), 10, 1500, 50, 10000, 2000, 3000)
+        self.assertEqual(data.year, 2019)
+        self.assertEqual(data.rst_date, date(2019, 11, 30))
+        self.assertEqual(data.miss, 20)
+        self.assertEqual(data.miss_rate, 20 / 20000)
+        self.assertEqual(data.miss_aoi, 20 / 2000)
+        self.assertEqual(data.overkill, 3000)
+        self.assertEqual(data.overkill_rate, 3000 / 20000)
+        self.assertEqual(data.overkill_aoi, 3000 / 3900)
+        self.assertEqual(data.useless, 100)
+        self.assertEqual(data.useless_rate, 100 / 20000)
+        self.assertEqual(data.aoi_in, 20000)
+        self.assertEqual(data.aoi_ng, 4000)
+        self.assertEqual(data.aoi_ok, 16000)
+        self.assertEqual(data.fi_in, 6000)
